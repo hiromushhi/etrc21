6d1e29dea5e830a37012d30bf0b10e409d379080
Ikeda Ryo <k6463102@kadai.jp>
2021-05-19 16:01:42 +0900

    add: MotorIo/SensorIo Update()

--- device_io.cpp
+++ device_io.cpp
@@ -8,10 +8,20 @@ MotorIo::MotorIo() {
 MotorIo::~MotorIo() {
 }
 
+void MotorIo::Update() {
+  counts_r_ = ev3_motor_get_counts(EV3_PORT_B);
+  counts_l_ = ev3_motor_get_counts(EV3_PORT_C);
+}
+
 SensorIo::SensorIo() {
   ev3_sensor_config(EV3_PORT_1, TOUCH_SENSOR);
   ev3_sensor_config(EV3_PORT_2, COLOR_SENSOR);
 }
 
 SensorIo::~SensorIo() {
+}
+
+void SensorIo::Update() {
+  touch_pressed_ = ev3_touch_sensor_is_pressed(EV3_PORT_1);
+  ev3_color_sensor_get_rgb_raw(EV3_PORT_2, &color_rgb_raw_);
 }
--- device_io.h
+++ device_io.h
@@ -7,12 +7,20 @@ class MotorIo {
  public:
   MotorIo();
   ~MotorIo();
+  void Update();
+
+  int32_t counts_r_;
+  int32_t counts_l_;
 };
 
 class SensorIo {
  public:
   SensorIo();
   ~SensorIo();
+  void Update();
+
+  bool touch_pressed_;
+  rgb_raw_t color_rgb_raw_;
 };
 
 #endif  // ETRC21_DEVICE_IO_H_
