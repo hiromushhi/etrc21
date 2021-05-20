2ffb9a11d2cde0cc68ea442d90a6d16f38ba735b
Ikeda Ryo <k6463102@kadai.jp>
2021-05-20 09:46:59 +0900

    add: utils

---
+++ utils.cpp
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+#include "app.h"
+
+PidControl::PidControl(float kp, float ki, float kd)
+    : kp_(kp), ki_(ki), kd_(kd), dt_(EXEC_ACTION_DT_MS/1000.0),
+      diff0_(0), diff1_(0), integral_(0) {
+}
+
+void PidControl::SetGain(float kp, float ki, float kd) {
+  kp_ = kp;
+  ki_ = ki;
+  kd_ = kd;
+}
+
+float PidControl::GetMv(int32_t current_val, int32_t target_val) {
+  float p, i, d, mv;
+
+  diff0_ = diff1_;
+  diff1_ = current_val - target_val;
+  integral_ += (diff1_ + diff0_) / 2.0 * dt_;
+
+  p = kp_ * diff1_;
+  i = ki_ * integral_;
+  d = kd_ * (diff1_ - diff0_) / dt_;
+  mv = p + i + d;
+
+  return mv;
+}
---
+++ utils.h
@@ -0,0 +1,22 @@
+#ifndef ETRC21_UTILS_H_
+#define ETRC21_UTILS_H_
+
+#include "ev3api.h"
+
+class PidControl {
+ public:
+  PidControl(float kp, float ki, float kd);
+  void SetGain(float kp, float ki, float kd);
+  float GetMv(int32_t current_val, int32_t target_val);
+
+ private:
+  float kp_;
+  float ki_;
+  float kd_;
+  float dt_;
+  float diff0_;
+  float diff1_;
+  float integral_;
+};
+
+#endif  // ETRC21_UTILS_H_
