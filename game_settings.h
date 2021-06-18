4a63112777f94ca4cca87d652b355fc5f5b787e4
Ikeda Ryo <k6463102@kadai.jp>
2021-06-18 11:11:02 +0900

    add: game_settings

---
+++ game_settings.h
@@ -0,0 +1,41 @@
+#ifndef ETRC21_GAME_SETTINGS_H_
+#define ETRC21_GAME_SETTINGS_H_
+
+const int kCircleNum = 28;
+
+const char* kLcourseCircleData[kCircleNum] = {
+  "A,-6,6,R",
+  "B,-2,6,R",
+  "C,2,6,B",
+  "D,6,6,B",
+  "E,-6,2,R",
+  "F,-2,2,R",
+  "G,2,2,B",
+  "H,6,2,B",
+  "J,-6,-2,Y",
+  "K,-2,-2,Y",
+  "L,2,-2,G",
+  "M,6,-2,G",
+  "P,-6,-6,Y",
+  "Q,-2,-6,Y",
+  "R,2,-6,G",
+  "S,6,-6,G",
+  "W,-9,-10,G",
+  "X,-2,-10,Y",
+  "1,-4,4,Y",
+  "2,0,4,G",
+  "3,4,4,R",
+  "4,-4,0,B",
+  "5,4,0,Y",
+  "6,-4,-4,G",
+  "7,0,-4,R",
+  "8,4,-4,B",
+  "9,0,0,N",
+  "0,-9,-5,R",
+};
+
+const char* kRcourseCircleData[] = {
+  "",
+};
+
+#endif  // ETRC21_GAME_SETTINGS_H_
