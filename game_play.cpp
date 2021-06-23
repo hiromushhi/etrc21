c4885916affc5faec8c5908bd63d0d593a9c58af
Ikeda Ryo <k6463102@kadai.jp>
2021-06-23 13:43:58 +0900

    add: BlockDecision NextCarryBlock()

--- game_play.cpp
+++ game_play.cpp
@@ -1,10 +1,46 @@
 #include "game_play.h"
 
+#include <limits>
+
+#include "ev3api.h"
+
+BlockDecision::BlockDecision(BingoArea* bingo_area) : bingo_area_(bingo_area) {
+}
+
+Block* BlockDecision::NextCarryBlock() {
+  Robot* robot = &bingo_area_->robot_;
+  Block* next_block = NULL;
+
+  double min = std::numeric_limits<double>::infinity();
+  const double kDecisionCoeff = 1.5;
+
+  for (int i = 0; i < kBlockNum; ++i) {
+    Block* block = &bingo_area_->blocks_[i];
+    if (block->carrying_completed)
+      continue;
+
+    double d = bingo_area_->DistanceBtwCircles(robot->circle, block->circle);
+    if (robot->circle->x != block->circle->x &&
+        robot->circle->y != block->circle->y) {
+      d *= kDecisionCoeff;
+    }
+
+    if (min > d) {
+      next_block = block;
+      min = d;
+    }
+  }
+
+  return next_block;
+}
+
 BingoAgent::BingoAgent(bool is_Rcourse) : is_Rcourse_(is_Rcourse) {
   bingo_area_ = new BingoArea(is_Rcourse_);
+  block_decision_ = new BlockDecision(bingo_area_);
 }
 
 BingoAgent::~BingoAgent() {
+  delete block_decision_;
   delete bingo_area_;
 }
 
@@ -14,4 +50,6 @@ void BingoAgent::SetBlockPos(BlockId block_id, char circle_id) {
 
 void BingoAgent::SolveBingo() {
   bingo_area_->UpdateBlockTarget();
+
+  Block* b = block_decision_->NextCarryBlock();
 }
--- game_play.h
+++ game_play.h
@@ -3,6 +3,15 @@
 
 #include "game_info.h"
 
+class BlockDecision {
+ public:
+  BlockDecision(BingoArea* bingo_area);
+  Block* NextCarryBlock();
+
+ private:
+  BingoArea* bingo_area_;
+};
+
 class BingoAgent {
  public:
   BingoAgent(bool is_Rcourse);
@@ -13,6 +22,7 @@ class BingoAgent {
  private:
   bool is_Rcourse_;
   BingoArea* bingo_area_;
+  BlockDecision* block_decision_;
 };
 
 #endif  // ETRC21_GAME_PLAY_H_
