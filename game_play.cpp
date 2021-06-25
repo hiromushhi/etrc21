8e0225d40d780562eb7759b434eaf754a6651b88
Ikeda Ryo <k6463102@kadai.jp>
2021-06-25 10:33:55 +0900

    feat: calculation moving route

--- game_play.cpp
+++ game_play.cpp
@@ -4,12 +4,77 @@
 
 #include "ev3api.h"
 
+const int kMax = std::numeric_limits<int>::max();
+
+RouteSearch::RouteSearch(BingoArea* bingo_area) : bingo_area_(bingo_area) {
+}
+
+void RouteSearch::ResetRouteSearchInfo() {
+  for (int i = 0; i < kCircleNum; ++i) {
+    Circle* circle = &bingo_area_->circles_[i];
+    circle->prev = NULL;
+    circle->is_fixed = false;
+
+    if ('1' <= circle->id && circle->id <= '9') {
+      circle->cost = kMax;
+    } else if (circle->block != NULL) {
+      circle->cost = kMax;
+    } else {
+      circle->cost = -1;
+    }
+  }
+}
+
+bool RouteSearch::CalcMovingRoute(Circle* goal_circle) {
+  static Circle* curr_circle = NULL;
+
+  if (!goal_circle->is_fixed) {
+    goal_circle->cost = 0;
+    goal_circle->is_fixed = true;
+    curr_circle = goal_circle;
+  }
+
+  static int i = 0;
+  Circle* next_circle = curr_circle->next[i];
+  Robot* robot = &bingo_area_->robot_;
+  static bool dead_end = true;
+
+  if (next_circle != NULL) {
+    if (next_circle->cost == -1 || next_circle->id == robot->circle->id) {
+      next_circle->cost = 0;
+      next_circle->prev = curr_circle;
+      dead_end = false;
+      if (next_circle->id == robot->circle->id) {
+        return true;
+      }
+    }
+  }
+
+  ++i;
+  if (i >= kNextToMax) {
+    i = 0;
+    curr_circle->is_fixed = true;
+    if (dead_end) {
+      curr_circle = curr_circle->prev;
+    }
+
+    for (int j = 0; j < kNextToMax; ++j) {
+      if (!curr_circle->next[j]->is_fixed && curr_circle->next[j]->cost != kMax) {
+        curr_circle = curr_circle->next[j];
+        dead_end = true;
+        break;
+      }
+    }
+  }
+  return false;
+}
+
 BlockDecision::BlockDecision(BingoArea* bingo_area) : bingo_area_(bingo_area) {
 }
 
 Block* BlockDecision::NextCarryBlock() {
   Robot* robot = &bingo_area_->robot_;
-  Block* next_block = NULL;
+  Block* next_carry_block = NULL;
 
   double min = std::numeric_limits<double>::infinity();
   const double kDecisionCoeff = 1.5;
@@ -26,20 +91,23 @@ Block* BlockDecision::NextCarryBlock() {
     }
 
     if (min > d) {
-      next_block = block;
+      next_carry_block = block;
       min = d;
     }
   }
 
-  return next_block;
+  return next_carry_block;
 }
 
-BingoAgent::BingoAgent(bool is_Rcourse) : is_Rcourse_(is_Rcourse) {
+BingoAgent::BingoAgent(bool is_Rcourse)
+    : is_Rcourse_(is_Rcourse), curr_step_(kCarryBlockUndecided), next_carry_block_(NULL) {
   bingo_area_ = new BingoArea(is_Rcourse_);
   block_decision_ = new BlockDecision(bingo_area_);
+  route_search_ = new RouteSearch(bingo_area_);
 }
 
 BingoAgent::~BingoAgent() {
+  delete route_search_;
   delete block_decision_;
   delete bingo_area_;
 }
@@ -48,8 +116,45 @@ void BingoAgent::SetBlockPos(BlockId block_id, char circle_id) {
   bingo_area_->SetBlockPos(block_id, circle_id);
 }
 
-void BingoAgent::SolveBingo() {
+void BingoAgent::UpdateBlockTarget() {
   bingo_area_->UpdateBlockTarget();
+}
 
-  Block* b = block_decision_->NextCarryBlock();
+void BingoAgent::TakeOneStep() {
+  switch (curr_step_) {
+    case kCarryBlockUndecided:
+      DecideNextCarryBlock();
+      break;
+
+    case kMovingRouteUnresolved:
+      SearchMovingRoute();
+      break;
+
+    default:
+      break;
+  }
+}
+
+void BingoAgent::DecideNextCarryBlock() {
+  next_carry_block_ = block_decision_->NextCarryBlock();
+
+  if (next_carry_block_ == NULL) {
+    curr_step_ = kBingoCompleted;
+  } else {
+    curr_step_ = kMovingRouteUnresolved;
+  }
+}
+
+void BingoAgent::SearchMovingRoute() {
+  static bool is_entry = true;
+  if (is_entry) {
+    route_search_->ResetRouteSearchInfo();
+    is_entry = false;
+  }
+
+  if (route_search_->CalcMovingRoute(next_carry_block_->circle)) {
+    curr_step_ = kCarryRouteUnresolved;
+    is_entry = true;
+    syslog(LOG_NOTICE, "transition kCarryRouteUnresolved");
+  }
 }
--- game_play.h
+++ game_play.h
@@ -3,6 +3,16 @@
 
 #include "game_info.h"
 
+class RouteSearch {
+ public:
+  RouteSearch(BingoArea* bingo_area);
+  void ResetRouteSearchInfo();
+  bool CalcMovingRoute(Circle* goal_circle);
+
+ private:
+  BingoArea* bingo_area_;
+};
+
 class BlockDecision {
  public:
   BlockDecision(BingoArea* bingo_area);
@@ -12,17 +22,30 @@ class BlockDecision {
   BingoArea* bingo_area_;
 };
 
+enum Step {
+  kCarryBlockUndecided = 0,
+  kMovingRouteUnresolved,
+  kCarryRouteUnresolved,
+  kBingoCompleted,
+};
+
 class BingoAgent {
  public:
   BingoAgent(bool is_Rcourse);
   ~BingoAgent();
   void SetBlockPos(BlockId block_id, char circle_id);
-  void SolveBingo();
+  void UpdateBlockTarget();
+  void TakeOneStep();
 
  private:
+  void DecideNextCarryBlock();
+  void SearchMovingRoute();
   bool is_Rcourse_;
+  Step curr_step_;
+  Block* next_carry_block_;
   BingoArea* bingo_area_;
   BlockDecision* block_decision_;
+  RouteSearch* route_search_;
 };
 
 #endif  // ETRC21_GAME_PLAY_H_
